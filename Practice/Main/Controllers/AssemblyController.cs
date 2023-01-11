22fcd94e15736c2912087fdc335e78f535822f40
Ali200119 <aliit@code.edu.az>
2023-01-11 04:06:35 +0400

    feat: Reflection

---
+++ Practice/Main/Controllers/AssemblyController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace Main.Controllers
+{
+	public class AssemblyController
+	{
+		public void ShowDatasViaAssembly()
+		{
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            foreach (var item in assembly.GetTypes())
+            {
+                foreach (var item2 in item.GetMembers())
+                {
+                    Console.WriteLine(item2);
+                }
+            }
+        }
+	}
+}
--- Practice/Main/Program.cs
+++ Practice/Main/Program.cs
@@ -1,9 +1,18 @@
-﻿using Main.Controllers;
+﻿using System.Reflection;
+using Main.Controllers;
 
 #region Indexer
 
 //LibraryController controller = new LibraryController();
 
 //controller.Show();
+
+#endregion
+
+#region Reflection
+
+//AssemblyController controller = new AssemblyController();
+
+//controller.ShowDatasViaAssembly();
 
 #endregion
